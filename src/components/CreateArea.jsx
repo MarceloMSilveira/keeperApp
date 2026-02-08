30cc7a20b1de9f472a1fe1a18c280d5099193f4c
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2026-02-08 20:04:49 -0300

    finish

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import AddTaskIcon from '@mui/icons-material/AddTask';
 import Fab from '@mui/material/Fab';
 import Zoom from '@mui/material/Zoom';
@@ -8,6 +8,8 @@ function CreateArea({sendNoteToApp}) {
     title: '',
     content: ''
   })
+  let [situacaoEspera, setSituacaoEspera] = useState(true)
+  const titleRef = useRef(null);
   
   function addFunction(evt) {
     evt.preventDefault()
@@ -16,14 +18,38 @@ function CreateArea({sendNoteToApp}) {
       title: '',
       content: ''
     })
+    setSituacaoEspera(true)
   }
+
+  useEffect(() => {
+    if (!situacaoEspera) {
+      titleRef.current?.focus();
+    }
+  }, [situacaoEspera]);
+
 
   return (
     <div>
       <form className="create-note">
-        <input name="title" placeholder="Title" onChange={evt => setNote({...note,[evt.target.name]:evt.target.value})} value={note.title}/>
-        <textarea name="content" placeholder="Take a note..." rows="3" onChange={evt => setNote({...note,[evt.target.name]:evt.target.value})} value={note.content}/>
-        <Zoom in={true}>
+        <input
+          ref={titleRef}
+          hidden={situacaoEspera}
+          name="title" 
+          placeholder="Title" 
+          onChange={evt => setNote({...note,[evt.target.name]:evt.target.value})} 
+          value={note.title}
+        />
+        <textarea 
+          name="content" 
+          placeholder="Take a note..." 
+          rows={situacaoEspera ? "1" : "3" }
+          onChange={evt => {
+            setSituacaoEspera(false)
+            setNote({...note,[evt.target.name]:evt.target.value})}} 
+          value={note.content}
+          onClick={()=>setSituacaoEspera(false)}
+        />
+        <Zoom in={!situacaoEspera}>
           <Fab onClick={addFunction}> 
             <AddTaskIcon />
           </Fab>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import './header.css'
+import HighlightIcon from '@mui/icons-material/Highlight';
 
 export default function Header() {
     return(
         <header>
-            <h1>MS1 Keeper App</h1>
+            <h1>
+                < HighlightIcon fontSize='large'/>
+                MS1 Keeper App</h1>
         </header>
     )
 }
