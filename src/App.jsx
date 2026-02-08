8e55f4c455507825b9b18e8e5e6bd1272f882b8f
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2026-02-07 21:35:40 -0300

    mostrar nova nota do createArea no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,26 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Note from './components/Note'
 import CreateArea from './components/CreateArea'
+//import { useState } from 'react'
 
 function App() {
+
+  // let [notes, setNotes] = useState ([])
   
+  // function addNoteToList(title, content) {
+    
+  // }
+
+  function mostrarNota(nota) {
+    console.log(nota.title)
+    console.log(nota.content)
+  }
+
 
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea showNote={mostrarNota}/>
       <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,23 @@
-function CreateArea() {
+import { useState } from "react";
+
+function CreateArea({showNote}) {
+  let [note, setNote] = useState({
+    title: '',
+    content: ''
+  })
+  
+  function addFunction(evt) {
+    evt.preventDefault()
+    //console.log(`titulo: ${note.title} e conteudo: ${note.content}`)
+    showNote(note)
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input name="title" placeholder="Title" onChange={evt => setNote({...note,[evt.target.name]:evt.target.value})} value={note.title}/>
+        <textarea name="content" placeholder="Take a note..." rows="3" onChange={evt => setNote({...note,[evt.target.name]:evt.target.value})} value={note.content}/>
+        <button onClick={addFunction} >Add</button>
       </form>
     </div>
   );
